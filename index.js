618ab16db0a696f0be81f04a44715d62b9452c40
kiba.x.zhao <kiba.rain@qq.com>
2021-01-08 17:29:08 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @fileOverview 目录文件
+ * @name index.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+/**
+ * @module xprovide
+ */
+
+const Provider = require('./lib/provider');
+const inject = require('./lib/inject');
+
+/** Provider. */
+exports.Provider = Provider;
+
+/** inject. */
+exports.inject = inject;
---
+++ xboot.js
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview xboot入口文件
+ * @name xboot.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+const { Provider, inject } = require('./index.js');
+const { BootLoader } = require('xboot');
+
+const loader = new BootLoader('xprovide.js');
+const provider = new Provider();
+
+loader.forEach(_ => inject(provider, _));
