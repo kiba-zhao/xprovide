a817b401a2e206f3630c07b598ee15d94ae1d1cc
kiba.x.zhao <kiba.rain@qq.com>
2021-01-13 22:14:04 +0800

    优化require pending项

--- test/lib/provider.test.js
+++ test/lib/provider.test.js
@@ -10,73 +10,108 @@ const Provider = require('@/lib/provider');
 
 describe('lib/provider', () => {
 
-  it('simple success', (done) => {
+  describe('simple', () => {
 
-    const provider = new Provider();
-    const ids = ['model1', 'model2', 'model3'];
-    const models = [];
+    it('success', done => {
 
-    for (let id of ids) {
-      const model = Symbol(id);
-      models.push(model);
-      provider.define(id, [], () => model);
-    }
+      const provider = new Provider();
+      const ids = [ 'model1', 'model2', 'model3' ];
+      const models = [];
 
-    provider.require(ids, (...args) => {
-      expect(args).toEqual(models);
-      done();
+      for (const id of ids) {
+        const model = Symbol(id);
+        models.push(model);
+        provider.define(id, [], () => model);
+      }
+
+      provider.require(ids, (...args) => {
+        expect(args).toEqual(models);
+        done();
+      });
+
+    });
+
+    it('success with require first', done => {
+
+      const provider = new Provider();
+      const ids = [ 'model1', 'model2', 'model3' ];
+      const models = [];
+
+      provider.require(ids, (...args) => {
+        expect(args).toEqual(models);
+        done();
+      });
+
+      for (const id of ids) {
+        const model = Symbol(id);
+        models.push(model);
+        provider.define(id, [], () => model);
+      }
+
+    });
+
+    it('require with dep', done => {
+
+      const provider = new Provider();
+      const ids = [ 'model1', 'model2', 'model3' ];
+      const factories = [];
+      const models = [];
+
+      provider.require(ids.slice(0, 1), m => {
+
+        expect(m).toEqual(models[0]);
+        expect(factories[0]).toBeCalledTimes(1);
+        expect(factories[0]).toBeCalledWith(...(models.slice(1)));
+        expect(factories[1]).toBeCalledTimes(1);
+        expect(factories[1]).toBeCalledWith(...(models.slice(2)));
+        expect(factories[2]).toBeCalledTimes(1);
+        expect(factories[2]).toBeCalledWith();
+
+        done();
+      });
+
+      for (let i = 0; i < ids.length; i++) {
+        const id = ids[i];
+        const model = Symbol(id);
+        models.push(model);
+        factories.push(jest.fn(() => model));
+        provider.define(id, ids.slice(i + 1), factories[i]);
+      }
+
+    });
+
+    it('require with single dep', done => {
+
+      const provider = new Provider();
+      const ids = [ 'model1', 'model2', 'model3' ];
+      const factories = [];
+      const models = [];
+
+      provider.require(ids.slice(0, 1), m => {
+
+        expect(m).toEqual(models[0]);
+        expect(factories[0]).toBeCalledTimes(1);
+        expect(factories[0]).toBeCalledWith(models[1]);
+        expect(factories[1]).toBeCalledTimes(1);
+        expect(factories[1]).toBeCalledWith(models[2]);
+        expect(factories[2]).toBeCalledTimes(1);
+        expect(factories[2]).toBeCalledWith();
+
+        done();
+      });
+
+      for (let i = 0; i < ids.length; i++) {
+        const id = ids[i];
+        const model = Symbol(id);
+        models.push(model);
+        factories.push(jest.fn(() => model));
+        provider.define(id, ids.slice(i + 1, i + 2), factories[i]);
+      }
+
     });
 
   });
 
-  it('simple success with require first', (done) => {
-
-    const provider = new Provider();
-    const ids = ['model1', 'model2', 'model3'];
-    const models = [];
-
-    provider.require(ids, (...args) => {
-      expect(args).toEqual(models);
-      done();
-    });
-
-    for (let id of ids) {
-      const model = Symbol(id);
-      models.push(model);
-      provider.define(id, [], () => model);
-    }
-
-  });
-
-  it('require with dep', (done) => {
-
-    const provider = new Provider();
-    const ids = ['model1', 'model2', 'model3'];
-    const factories = [];
-    const models = [];
-
-    provider.require(ids.slice(0, 1), (m) => {
-
-      expect(m).toEqual(models[0]);
-      expect(factories[0]).toBeCalledTimes(1);
-      expect(factories[0]).toBeCalledWith(...(models.slice(1)));
-      expect(factories[1]).toBeCalledTimes(1);
-      expect(factories[1]).toBeCalledWith(...(models.slice(2)));
-      expect(factories[2]).toBeCalledTimes(1);
-      expect(factories[2]).toBeCalledWith();
-
-      done();
-    });
-
-    for (let i = 0; i < ids.length; i++) {
-      const id = ids[i];
-      const model = Symbol(id);
-      models.push(model);
-      factories.push(jest.fn(() => model));
-      provider.define(id, ids.slice(i + 1), factories[i]);
-    }
-
-  });
 
 });
 
--- typings/lib/provider.d.ts
+++ typings/lib/provider.d.ts
@@ -4,17 +4,19 @@ export = Provider;
  * @class
  */
 declare class Provider {
-    constructor(store: any, opts?: {
+    constructor(store?: {}, opts?: {
         cache: boolean;
     });
     require(deps: any, complete: any, fatal: any): void;
     define(id: any, deps: any, factory: any, opts?: {}): void;
-    [STORE]: any;
+    [PENDING_COUNT]: number;
+    [STORE]: {};
     [OPTS]: {
         cache: boolean;
     };
     [PENDINGS]: {};
 }
+declare const PENDING_COUNT: unique symbol;
 declare const STORE: unique symbol;
 declare const OPTS: unique symbol;
 declare const PENDINGS: unique symbol;
