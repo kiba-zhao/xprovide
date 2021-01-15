1bb20286c04fc164e3a3904827ff85f5a87b8458
kiba.x.zhao <kiba.rain@qq.com>
2021-01-15 16:11:23 +0800

    添加xboot测试代码

---
+++ test/xboot.test.js
@@ -0,0 +1,49 @@
+/**
+ * @fileOverview xboot引导测试文件
+ * @name xboot.test.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+describe('xboot', () => {
+
+  let Provider = jest.fn();
+  let Loader = jest.fn();
+
+  beforeAll(() => {
+    jest.doMock('../lib/provider', () => Provider, { virtual: true });
+    jest.doMock('xboot', () => ({ BootLoader: Loader }), { virtual: true });
+  });
+
+  afterEach(() => {
+    Provider.mockReset();
+    Loader.mockReset();
+
+    jest.resetModules();
+  });
+
+  afterAll(() => {
+    jest.dontMock('../lib/provider');
+    jest.dontMock('xboot');
+  });
+
+  it('simple', () => {
+    const provider = { test: 123 };
+    const modules = [jest.fn(), jest.fn()];
+    Provider.mockImplementation(() => provider);
+    Loader.mockImplementation(() => modules.map(_ => ({ content: _ })));
+
+    require('@/xboot');
+
+    expect(Loader).toBeCalledTimes(1);
+    expect(Loader).toBeCalledWith('xprovide.js');
+    expect(Provider).toBeCalledTimes(1);
+    expect(Provider).toBeCalledWith();
+    for (let fn of modules) {
+      expect(fn).toBeCalledTimes(1);
+      expect(fn).toBeCalledWith(provider);
+    }
+  });
+
+});
